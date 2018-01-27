cefb50f5590f05d85560b2f45a1e6c6d16c4f266
Naim Jeem <naim36208@gmail.com>
2018-01-27 23:20:07 +0600

    Get Client data from Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+// AngularFire2
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database-deprecated';
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -13,12 +17,23 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
+// Services
+import { ClientService } from './services/client.service';
 
 const appRoutes: Routes = [
-  { path: '',         component: DashboardComponent },
-  { path: 'register', component: RegisterComponent  },
-  { path: 'login',    component: LoginComponent     }
+  { path: '',               component: DashboardComponent },
+  { path: 'register',       component: RegisterComponent  },
+  { path: 'login',          component: LoginComponent     },
+  { path: 'add-client',     component: AddClientComponent }
 ];
+
+export const firebaseConfig = {
+  apiKey: "*************",
+  authDomain: "**************",
+  databaseURL: "*************",
+  storageBucket: "*********",
+  messagingSenderId: "********"
+};
 
 @NgModule({
   declarations: [
@@ -36,9 +51,14 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import { Observable } from 'rxjs';
+import { Client } from '../models/client.interface'
+
+@Injectable()
+export class ClientService {
+  clients: FirebaseListObservable<any[]>;
+  client: FirebaseObjectObservable<any[]>;
+
+  constructor(
+    public af: AngularFireDatabase
+  ) {
+    this.clients = this.af.list('/clients') as FirebaseListObservable<Client[]>;
+  }
+
+  getClients() {
+    return this.clients;
+  }
+
+}
